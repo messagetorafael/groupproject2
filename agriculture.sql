6f41db51b1be2a3a2c1778339ff332a8b94e6835
WOIR <james_dietz@yahoo.com>
2019-05-18 11:58:42 -0500

    SQL code changes, no other code changes to other files.

--- agriculture.sql
+++ agriculture.sql
@@ -1,6 +1,14 @@
+-- Rafael Santos and James Dietz
+-- ETL project, May 18, 2019
+-- agriculture data
+
+-- CREATE DATABASE
+-- create and use database to store data 
 CREATE DATABASE agriculture_db;
 USE agriculture_db;
-  
+
+-- CREATE TABLES
+-- create a table to hold df on cereal yield, World Bank data  
 CREATE TABLE cereal_yield (
   country_code VARCHAR(3),
   country_name TEXT,
@@ -8,26 +16,28 @@ CREATE TABLE cereal_yield (
   PRIMARY KEY (country_code)
   );
  
+ -- create a table to hold df on agriculture land, World Bank data.  Had to drop table and recreate to deal
+ -- with problem of SQL not accepting decimal with 6 decimal levels.
   CREATE TABLE agriculture_land (
   country_code VARCHAR(3),
   country_name TEXT,
   percent_land DECIMAL(8,6),
   PRIMARY KEY (country_code)
   );
+  
+  -- Examination of tables, drops and alters were used to resolve problems with decimal places in cereal_yield
 DROP TABLE agriculture_land;  
 ALTER TABLE agriculture_land
-	MODIFY COLUMN percent_land DECIMAL(20,16)
-    ;
+	MODIFY COLUMN percent_land DECIMAL(20,16);
   
   SELECT * FROM cereal_yield;
   SELECT * FROM agriculture_land;
-  
   
   DESCRIBE cereal_yield;
   DESCRIBE agriculture_land;
   
   
-       
+  -- This table was created to store the data having to do with specific commodity yields and prices related to cereal crops.     
   CREATE TABLE table_food (
   country_code VARCHAR(3),
   country TEXT,
@@ -44,5 +54,39 @@ ALTER TABLE agriculture_land
   PRIMARY KEY (country_code)
   );
   
-  SELECT * FROM table_food
-  ;
+  SELECT * FROM table_food;
+  
+  -- TABLE JOINS
+  
+  -- A left join and inner join (depends on the analyses desired) yields 215 rows of data
+  SELECT cereal_yield.country_name, cereal_yield.country_code, cereal_yield.yield, agriculture_land.percent_land, 
+	  table_food.currency_ID, commodity_purchased_bread, table_food.unit_of_goods_measurement_bread, 
+	  table_food.monthly_average_price_paid_bread, table_food.commodity_purchased_rice, 
+	  table_food.unit_of_goods_measurement_rice, table_food.monthly_average_price_paid_rice, 
+	  table_food.commodity_purchased_wheat, table_food.unit_of_goods_measurement_wheat, 
+      table_food.monthly_average_price_paid_wheat
+	FROM cereal_yield
+	LEFT JOIN agriculture_land 
+    ON cereal_yield.country_code = agriculture_land.country_code
+    JOIN table_food
+    ON agriculture_land.country_code = table_food.country_code;
+    
+    -- Joining just two tables from World Bank Data, tables with the most available data (264 rows), may be 
+    -- desirable depending on the data analyses to be performed.
+	SELECT cereal_yield.country_name, cereal_yield.country_code, cereal_yield.yield, agriculture_land.percent_land 
+	FROM cereal_yield
+	JOIN agriculture_land 
+    ON cereal_yield.country_code = agriculture_land.country_code;
+    
+    -- Inner join on all three tables.
+    SELECT cereal_yield.country_name, cereal_yield.country_code, cereal_yield.yield, agriculture_land.percent_land, 
+	  table_food.currency_ID, commodity_purchased_bread, table_food.unit_of_goods_measurement_bread, 
+	  table_food.monthly_average_price_paid_bread, table_food.commodity_purchased_rice, 
+	  table_food.unit_of_goods_measurement_rice, table_food.monthly_average_price_paid_rice, 
+	  table_food.commodity_purchased_wheat, table_food.unit_of_goods_measurement_wheat, 
+      table_food.monthly_average_price_paid_wheat
+	FROM table_food
+    JOIN cereal_yield
+    ON table_food.country_code = cereal_yield.country_code
+    JOIN agriculture_land
+    ON cereal_yield.country_code = agriculture_land.country_code;
